7089a4e1c5bd88c0967e7f934434c47c3cd689a3
slamet kristanto <botoksgonzales@gmail.com>
2022-11-19 12:01:07 +0900

    Clean unnecessary dependencies

--- mix.exs
+++ mix.exs
@@ -21,12 +21,7 @@ defmodule AudioPlayer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
-      {:git_hooks, "~> 0.7.0", only: [:dev], runtime: false},
-      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:rustler, "~> 0.26.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
