be73da8ffc5ee655c1ef8ed1f57dbf54b57f109f
slamet kristanto <botoksgonzales@gmail.com>
2022-11-21 07:07:35 +0900

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AudioPlayer.MixProject do
   def project do
     [
       app: :audio_player,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
