f5aa5e28a14125ae0753fdd5e11dea04eeb5e559
slamet kristanto <botoksgonzales@gmail.com>
2022-11-21 07:55:25 +0900

    Update checksum and remove unnecessary file

--- checksum-Elixir.AudioPlayer.exs
+++ checksum-Elixir.AudioPlayer.exs
@@ -1,3 +1,10 @@
 %{
+  "audio_player-v0.2.1-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:76036fdc4ad9316d552e2c638c6f40e760b86e9fe12840f12fdf4dbb549b0d2f",
+  "audio_player-v0.2.1-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:255b516ecf188e0b1f7de4d600f0f74ec13f602532b011f65f917daf9da5020a",
+  "libaudio_player-v0.2.1-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:659f65c2a69c556ccfdfea54cc3d56e0008d3699ff80e7ee45c23fe99c5aee08",
+  "libaudio_player-v0.2.1-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:cd43fa16d0f2529caf77b56945886f391a95316a2d5a6c70bd63401147d15e32",
   "libaudio_player-v0.2.1-nif-2.16-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:db6a9c31468b3a170ac2b562aa69296e6631efb7738bed0e651876e66efbc7df",
+  "libaudio_player-v0.2.1-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:bd63b0fc3c6b8612e0ecad1eed5d2d311d361f9399aa3ee917653f38254e3020",
+  "libaudio_player-v0.2.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:30ab0812191022f650f3f826f746f3c0f73db078aa042850835f37ff3732b16f",
+  "libaudio_player-v0.2.1-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:7a474673fd264d01cc30c392d9ab349d31a061af7ec113a36151424f584a515a",
 }
